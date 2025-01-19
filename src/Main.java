fa7e854ec52616299a1ac447d760f7939505a222
MohammadMehdi khorshidi <0911a852a85a79@gmail.com>
2025-01-19 20:50:41 +0330

    output.log added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 public class Main {
     public static void main(String[] args) {
         Management m = new Management();
 
+                try {
+                    PrintStream fileOut = new PrintStream(new FileOutputStream("src/output.log", true));
+                    System.setOut(fileOut);
+                    System.setErr(fileOut);
 
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
     }
 }
